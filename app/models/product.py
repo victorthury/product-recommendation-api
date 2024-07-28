e4575ab5d28bf6edc2741ff9d506cb9b0e86352d
Victor Thury <victorhauache@gmail.com>
2024-07-28 18:49:27 -0400

    add: product model and reading product data

---
+++ app/models/product.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+import pandas as pd
+
+class Product(BaseModel):
+  product_id: int
+  product_title: str
+  product_price: float
+  product_image_url: str
+  store_id: int
+  store_name: str
+
+def get_products():
+  path = './xpto_sales_products_mar_may_2024.csv'
+
+  products = pd.read_csv(path)
+  return products
